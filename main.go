23ff7b7a83321fdc605aebe6e999f017682ab888
Ryoji Kobori <kobori75@gmail.com>
2019-01-10 02:02:11 +0900

    リスト描画とCSSの適用

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import "github.com/gin-gonic/gin"
+import "os"
+
+type User struct {
+	Name string
+	Age int
+	Note string
+}
 
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*")
+	r.Static("/css", "assets/css")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.tmpl", gin.H{
-			"title": "Hello, world",
-		})
+		index(c)
 	})
-	r.Run(":5050")
+
+	port := os.Getenv("PORT")
+  if len(port) == 0 {
+      port = "3000"
+  }
+  r.Run(":" + port)
+}
+
+func index(c *gin.Context) {
+	users := []User{User{"abc", 21, ""}, User{"def", 34, "xcv"}, User{"ghi", 8, "12489"}}
+	c.HTML(200, "index.tmpl", gin.H{
+		"title": "Hello, world",
+		"users": users,
+	})
 }
