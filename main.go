2a4b1341d1ffc643244fd58d62df112cbe0a34a0
Ryoji Kobori <kobori75@gmail.com>
2019-01-11 03:07:46 +0900

    ルーティング設定

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "os"
 
 type User struct {
 	Name string
-	Age int
+	Age  int
 	Note string
 }
 
@@ -14,21 +14,37 @@ func main() {
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/css", "assets/css")
 
-	r.GET("/", func(c *gin.Context) {
+	setRoute(r)
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+	r.Run(":" + port)
+}
+
+func setRoute(router *gin.Engine) {
+	router.GET("/", func(c *gin.Context) {
 		index(c)
 	})
 
-	port := os.Getenv("PORT")
-  if len(port) == 0 {
-      port = "3000"
-  }
-  r.Run(":" + port)
+	router.GET("/sub", func(c *gin.Context) {
+		sub(c)
+	})
 }
 
 func index(c *gin.Context) {
-	users := []User{User{"abc", 21, ""}, User{"def", 34, "xcv"}, User{"ghi", 8, "12489"}}
+	users := []User{{"abc", 21, ""}, {"def", 34, "xcv"}, {"ghi", 8, "12489"}}
 	c.HTML(200, "index.tmpl", gin.H{
 		"title": "Hello, world",
+		"users": users,
+	})
+}
+
+func sub(c *gin.Context) {
+	users := []User{{"hoge", 21, ""}, {"fuga", 34, "xcv"}, {"piyo", 8, "12489"}}
+	c.HTML(200, "index.tmpl", gin.H{
+		"title": "This is Sub",
 		"users": users,
 	})
 }
